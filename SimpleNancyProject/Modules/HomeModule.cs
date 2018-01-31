2ec921592e428cecc39458be02cf2580438e424b
Łukasz Kierepka <lukasz_kierepka@hotmail.com>
2018-01-31 11:47:39 +0100

    Dodanie nancy do projektu. Dodanie HomeModule

---
+++ SimpleNancyProject/Modules/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace SimpleNancyProject.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args => "Hello from Nancy");
+        }
+    }
+}
